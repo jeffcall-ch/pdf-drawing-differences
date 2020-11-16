e94401bea0e0cd9d238e3a240e1335919293c671
Sziva Laszlo <Laszlo.Sziva@ansaldoenergia.com>
2020-11-16 16:13:50 +0100

    work on

--- get-jpg-from-pdf.py
+++ get-jpg-from-pdf.py
@@ -1,4 +1,5 @@
 from pdf2image import convert_from_path
+from scipy.ndimage import interpolation
 from skimage.measure import compare_ssim
 import imutils
 import cv2
@@ -47,39 +48,6 @@ for pdf_file in input_output_pdf_list:
     print (f"jpeg resolution - height x width: {height} x {width}")
 
 
-def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
-
-    # if both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return image
-
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation = inter)
-
-    # return the resized image
-    return resized
-
-
-
 # check which - old or new jpegs - height is smaller, if any for the resizing to happen
 print (f"old hei: {list_of_resolutions[0][0]}")
 print (f"new hei: {list_of_resolutions[1][0]}")
@@ -90,19 +58,55 @@ if old_height > new_height:
     # resize the old pictures
     print ("resizing old images")
     for currentImage in list_of_CV2_image_lists[0]:
-        image_resize(currentImage, height = new_height)
+        # note, that the above extracted shape tuple is in a different order as the resize function wants. Therefore reversed for usage...
+        currentImage = cv2.resize(currentImage, (list_of_resolutions[1][1],list_of_resolutions[1][0]), interpolation = cv2.INTER_AREA)
         cv2.imwrite(r"C:\Users\50000700\Python\Python_repos\pdf-drawing-differences\pdf\temp\resized.jpg", currentImage)
 elif new_height > old_height:
     # resize the new pictures
     print ("resizing new images")
     for currentImage in list_of_CV2_image_lists[1]:
-        image_resize(currentImage, height = old_height)
+        # note, that the above extracted shape tuple is in a different order as the resize function wants. Therefore reversed for usage...
+        currentImage = cv2.resize(currentImage, (list_of_resolutions[0][1],list_of_resolutions[0][0]), interpolation = cv2.INTER_AREA)
         cv2.imwrite(r"C:\Users\50000700\Python\Python_repos\pdf-drawing-differences\pdf\temp\resized.jpg", currentImage)
 else :
     # if the hei are the same for old and new
     pass
 
 
+# print transparent overlays
 
+for idx, current_old_image in enumerate(list_of_CV2_image_lists[0]):
+    current_new_image = list_of_CV2_image_lists[1][idx]
 
+    # convert images to grayscale
+    gray_old = cv2.cvtColor(current_old_image, cv2.COLOR_BGR2GRAY)
+    gray_new = cv2.cvtColor(current_new_image, cv2.COLOR_BGR2GRAY)
 
+    # compute structural similarity index (SSIM)
+    (score, diff) = compare_ssim(gray_old, gray_new, full=True)
+    diff = (diff * 255).astype("uint8")
+    # value can fall between -1 to 1, where 1 is the perfect match
+    print("SSIM: {}".format(score))
+
+    # treshold the difference image, followed by finding contours to
+    # obtain the regions of the two images that differ
+    thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+
+    # copy input picture, as overlay has to be on different picture
+    new_overlay = current_new_image.copy()
+    # define output picture
+    output = current_new_image.copy()
+    alpha = 0.2
+
+    for c in cnts:
+        # compute the bounding box of the contour and then draw the
+        # bounding box on both input images to represent where the two
+        # images differ
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(new_overlay, (x, y), (x + w, y + h), (0, 0, 255), -1)
+
+    cv2.addWeighted(new_overlay, alpha, current_new_image, 1 - alpha, 0, output)
+
+    cv2.imwrite(r"C:\Users\50000700\Python\Python_repos\pdf-drawing-differences\pdf\temp\output.jpg", output)
